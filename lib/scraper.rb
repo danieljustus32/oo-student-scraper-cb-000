5688fd7ae5910d6d4831e98bae66dec6a755df8e
Daniel Justus <daniel.justus32@gmail.com>
2017-10-09 03:45:51 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,40 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open("#{index_url}"))
+    students = []
+    doc.css(".student-card").each do | card |
+      name = card.css(".student-name").text
+      location = card.css(".student-location").text
+      profile_url = card.css("a").first["href"]
+      students << {:name=>name,
+                   :location=>location,
+                   :profile_url=>profile_url}
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    doc = Nokogiri::HTML(open("#{profile_url}"))
+    urls = doc.css(".social-icon-container a").map { |link| link["href"]}
+    twitter = urls.detect { |url| url =~ /twitter/}
+    linkedin = urls.detect { |url| url =~ /linkedin/}
+    github = urls.detect { |url| url =~ /github/}
+    profile_quote = doc.css(".profile-quote").text
+    bio = doc.css(".description-holder p").text
+    info = {:twitter=>twitter,
+            :linkedin=>linkedin,
+            :github=>github,
+            :blog=>urls[3],
+            :profile_quote=>profile_quote,
+            :bio=>bio
+            }
+    info.delete_if { |k, v| v == nil}
+    info
   end
 
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,28 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    @name = student_hash[:name]
+    @location = student_hash[:location]
+    self.save
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each { |student| self.new(student) }
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each { |k, v| self.send("#{k}=", "#{v}")}
   end
 
   def self.all
-    
+    @@all
+  end
+
+  def save
+    @@all << self
   end
 end
-
